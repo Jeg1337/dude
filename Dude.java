6fc816b2718d51251bb6d2d6bb98efc9243e2295
Jeg1337 <49483489+Jeg1337@users.noreply.github.com>
2019-11-21 01:05:10 +0300

    достойно уважения, саня

---
+++ Dude.java
@@ -0,0 +1,10 @@
+class Dude {
+    Cat first = new Cat();
+    Dog second = new Dog();
+
+	public void call() {
+        System.out.println("рейнджеры, объединяемся!!!!");
+        first.meow();
+        second.bark();
+    }
+}
---
+++ Testmain.java
@@ -0,0 +1,6 @@
+class Testmain {
+    public static void main(String[] args) {
+        Dude test = new Dude();
+        test.call();
+    }
+}
